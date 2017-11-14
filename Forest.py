74134a4344aa8703fff596832877a4eab50e3699
Unknown <jautz@uos.de>
2017-11-14 17:32:07 +0100

    Started using sklearn for the learners, might be able to cut away the custom tree

--- Forest.py
+++ Forest.py
@@ -1,7 +1,10 @@
 import numpy as np
 from Tree import Tree
 from scipy.special import comb
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
 #Subset modes
 ALL="ALL"
 RANDOM="RANDOM"
@@ -24,6 +27,7 @@ class Forest:
         self.variant=variant
         self.input_data=None
         self.targets=None
+        self.saved_trees={}
 
     def fit(self,input_array:np.ndarray,targets:np.array):
         # Maybe not what is really needed:
@@ -46,7 +50,7 @@ class Forest:
     def predict(self,input_array:np.ndarray):
         if self.variant==ADHOC:
             return self._predict_ADHOC(input_array)
-        elif self.variant== BOOSTED
+        elif self.variant== BOOSTED:
             return self._predict_BOOSTED(input_array)
 
 
@@ -61,7 +65,6 @@ class Forest:
 
     def _predict_ADHOC(self, input_array):
         #Select matching subset of data
-        saved_trees={}
         results=[]
 
         for input in input_array: #Todo: Iterate over patterns instead of inputs
@@ -75,19 +78,49 @@ class Forest:
             forest.fit(matching_input,matching_targets)
             result=forest.predict([input])
 
-            saved_trees[str(pattern)]=forest
+            self.saved_trees[str(pattern)]=forest #Todo: Make use of saved trees
             results.append(result)
 
         return results
 
-
     def _fit_BOOSTED(self, input_array, targets):
-        #
+        # Generate random forest (Split data into train forest/train learner?
+            # Resample data
+        number_of_unique_patterns=len(np.unique(np.isnan(input_array), axis=1))
+        #idea: Number of trees equals to number of patterns=>
+        #More complicates data requires more complicated model <==> higher number of trees.
+        for i in range(number_of_unique_patterns): # Todo: Parrallize
+            # Choose feature subset
+            number_of_features= np.math.sqrt(input_array.shape[1]) #Root of features is standart ->wiki link
+            selected_features=np.random.choice(input_array.shape[1],number_of_features,replacement=False)
+            # Draw data where None of the features are nan, reduce features to selected
+            sub_feature_array = input_array[:, selected_features]
+            mask = np.isnan(sub_feature_array).any(axis=1)
+            mask = np.invert(mask, dtype=bool)
 
-        pass
+            viable_input=input_array[mask][:,selected_features]
+            viable_targets=targets[mask]
+
+            subsampled_input = viable_input[np.random.choice(viable_input,len(viable_input))]
+            subsampled_targets = viable_targets[np.random.choice(viable_targets,len(viable_targets))]
+
+            # Generate trees:
+            # Can use sklearn decison trees, must handle value error prior, and turn => turn nan error (Value Error) to special value!
+            self.trees = [] # use list instead of dicts
+            next_tree = DecisionTreeClassifier()
+            next_tree.fit(subsampled_input,subsampled_targets)
+            self.trees.append(next_tree)
+
+        # Create and train learner
+
+            # Connect trainer to trees
+
+            # Train learner
+
 
 
     def _predict_BOOSTED(self, input_array):
+        # Can use sklearn decison trees, must handle value error prior, and turn => turn nan error(Value Error ) to special value!
         pass
 
 
