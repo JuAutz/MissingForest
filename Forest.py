4e0989c360a47fe8c2e789b5d28cd30921153283
Unknown <jautz@uos.de>
2017-12-26 11:56:28 +0100

    Added evaluation and data generation, handled some edge cases

--- Forest.py
+++ Forest.py
@@ -4,59 +4,65 @@ from scipy.special import comb
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
 
-#Subset modes
-ALL="ALL"
-RANDOM="RANDOM"
+# Subset modes
+ALL = "ALL"
+RANDOM = "RANDOM"
 
-#Variants
-ADHOC="ADHOC"
-BOOSTED="BOOSTED"
+# Variants
+ADHOC = "ADHOC"
+BOOSTED = "BOOSTED"
 
+# Tokens
+NAN = "NaN"
 from logging import Logger
-log=Logger(__file__)
+
+log = Logger(__file__)
+
 
 class Forest:
+    def __init__(self, max_depth=None, cores=None, bootstrap_mode: str = ALL, resample_mode=None, variant: str = ADHOC):
+        self.max_depth = max_depth
+        self.cores = cores
+        self.bootstrap_mode = bootstrap_mode
+        self.resample_mode = resample_mode
+        self.trees = {}
+        self.variant = variant
+        self.input_data = None
+        self.targets = None
+        self.saved_trees = {}
+        if self.variant == BOOSTED:
+            self.svm = SVC()
+            self.encoder = LabelEncoder()
 
-    def __init__(self, max_depth=None, cores=None, bootstrap_mode:str= ALL,resample_mode=None,variant:str=ADHOC):
-        self.max_depth=max_depth
-        self.cores=cores
-        self.bootstrap_mode=bootstrap_mode
-        self.resample_mode=resample_mode
-        self.trees=[]
-        self.variant=variant
-        self.input_data=None
-        self.targets=None
-        self.saved_trees={}
-
-    def fit(self,input_array:np.ndarray,targets:np.array):
+    def fit(self, input_array: np.ndarray, targets: np.array):
         # Maybe not what is really needed:
         # Live computation of the models, only store the inputs.=v
         # Use the data within the input data that fits the missing value pattern,
         # bag it and predict using the resulting forest?
-        if self.variant==ADHOC:
-            self._fit_ADHOC(input_array,targets)
-        elif self.variant==BOOSTED:
-            self._fit_BOOSTED(input_array,targets)
+        if self.variant == ADHOC:
+            self._fit_ADHOC(input_array, targets)
+        elif self.variant == BOOSTED:
+            self._fit_BOOSTED(input_array, targets)
         else:
-            pass #Todo: Add the other variants
+            pass  # Todo: Add the other variants
 
-        # subsets=self._generate_subsets(input_array,targets)
-        # for subset in subsets:
-        #     next_tree=Tree(max_depth=self.max_depth,cores=self.cores)
-        #     next_tree.fit(subset[0],subset[1])
-        #     self.trees.append(next_tree)
+            # subsets=self._generate_subsets(input_array,targets)
+            # for subset in subsets:
+            #     next_tree=Tree(max_depth=self.max_depth,cores=self.cores)
+            #     next_tree.fit(subset[0],subset[1])
+            #     self.trees.append(next_tree)
 
-    def predict(self,input_array:np.ndarray):
-        if self.variant==ADHOC:
+    def predict(self, input_array: np.ndarray):
+        if self.variant == ADHOC:
             return self._predict_ADHOC(input_array)
-        elif self.variant== BOOSTED:
+        elif self.variant == BOOSTED:
             return self._predict_BOOSTED(input_array)
 
-
-
     def _fit_ADHOC(self, input_array, targets):
-        if self.input_data  is None and self.targets is None:
+        if self.input_data is None and self.targets is None:
             self.input_data = input_array
             self.targets = targets
         else:
@@ -64,73 +70,109 @@ class Forest:
             raise RuntimeError("Forest already fed with data")
 
     def _predict_ADHOC(self, input_array):
-        #Select matching subset of data
-        results=[]
+        # Select matching subset of data
+        results = []
 
-        for input in input_array: #Todo: Iterate over patterns instead of inputs
+        for input in input_array:  # Todo: Iterate over patterns instead of inputs
 
-            pattern=np.isnan(input)
+            pattern = np.isnan(input)
 
-            #Select the part of the saved data that matches the pattern of missing values of the input
-            matching_input=self.input_data[np.where((np.isnan(self.input_data)==pattern).all(axis=1))]
-            matching_targets=self.targets[np.where((np.isnan(self.input_data)==pattern).all(axis=1))]
-            forest=RandomForestClassifier() #Todo: Parameters??
-            forest.fit(matching_input,matching_targets)
-            result=forest.predict([input])
+            # Select the part of the saved data that matches the pattern of missing values of the input
+            matching_input = self.input_data[np.where((np.isnan(self.input_data) == pattern).all(axis=1))]
+            matching_targets = self.targets[np.where((np.isnan(self.input_data) == pattern).all(axis=1))]
+            forest = RandomForestClassifier()  # Todo: Parameters??
+            no_nan_matching_input = matching_input[:, ~pattern]
+            if no_nan_matching_input.shape[1] > 0:
+                forest.fit(no_nan_matching_input, matching_targets)  #
 
-            self.saved_trees[str(pattern)]=forest #Todo: Make use of saved trees
-            results.append(result)
+                no_nan_input = input[~pattern]
+                result = forest.predict(no_nan_input.reshape(1, -1))[0]  # Due to single sample
+
+                self.saved_trees[str(pattern)] = forest  # Todo: Make use of saved trees
+                results.append(result)
+            else:  # Todo: What do i do with all nan values??
+                results.append("RANDOM")
 
         return results
 
     def _fit_BOOSTED(self, input_array, targets):
         # Generate random forest (Split data into train forest/train learner?
-            # Resample data
-        number_of_unique_patterns=len(np.unique(np.isnan(input_array), axis=1))
-        #idea: Number of trees equals to number of patterns=>
-        #More complicates data requires more complicated model <==> higher number of trees.
-        for i in range(number_of_unique_patterns): # Todo: Parrallize
+        # Resample data
+        number_of_unique_patterns = len(np.unique(np.isnan(input_array), axis=1))
+        # idea: Number of trees equals to number of patterns=>
+        # More complicates data requires more complicated model <==> higher number of trees.
+        for i in range(number_of_unique_patterns):  # Todo: Parallize
             # Choose feature subset
-            number_of_features= np.math.sqrt(input_array.shape[1]) #Root of features is standart ->wiki link
-            selected_features=np.random.choice(input_array.shape[1],number_of_features,replacement=False)
+            number_of_features = int(np.math.sqrt(input_array.shape[1]))  # Root of features is standard ->wiki link
+            selected_features = np.random.choice(input_array.shape[1], number_of_features, replace=False)
             # Draw data where None of the features are nan, reduce features to selected
             sub_feature_array = input_array[:, selected_features]
             mask = np.isnan(sub_feature_array).any(axis=1)
             mask = np.invert(mask, dtype=bool)
 
-            viable_input=input_array[mask][:,selected_features]
-            viable_targets=targets[mask]
+            viable_input = input_array[mask][:, selected_features]
+            viable_targets = targets[mask]
 
-            subsampled_input = viable_input[np.random.choice(viable_input,len(viable_input))]
-            subsampled_targets = viable_targets[np.random.choice(viable_targets,len(viable_targets))]
+            subsampled_input = viable_input[np.random.choice(range(len(viable_input)), len(viable_input))]
+            subsampled_targets = viable_targets[np.random.choice(range(len(viable_targets)), len(viable_targets))]
 
             # Generate trees:
-            # Can use sklearn decison trees, must handle value error prior, and turn => turn nan error (Value Error) to special value!
-            self.trees = [] # use list instead of dicts
             next_tree = DecisionTreeClassifier()
-            next_tree.fit(subsampled_input,subsampled_targets)
-            self.trees.append(next_tree)
+            next_tree.fit(subsampled_input, subsampled_targets)
+            self.trees[tuple(selected_features)] = next_tree
 
-        # Create and train learner
+            # Create and train learner
 
-            # Connect trainer to trees
+            # Generate learner data: (samples,output_of_trees) as x, target class as y
+            learner_input = self._evaluate_trees(input_array)
+            assert len(learner_input) == len(targets)  # Todo: Check this in detail
 
             # Train learner
 
-
+            # Flatten for encoding, unflatten for learning
+            input_shape = learner_input.shape
+            flat_input = learner_input.flatten()
+            self.encoder.fit(flat_input)
+            encoded_input = self.encoder.transform(flat_input)
+            encoded_input = encoded_input.reshape(input_shape)
+            self.svm.fit(encoded_input, targets)  # Todo Something like epochs? Check N-iterations
 
     def _predict_BOOSTED(self, input_array):
-        # Can use sklearn decison trees, must handle value error prior, and turn => turn nan error(Value Error ) to special value!
-        pass
+        learner_input = self._evaluate_trees(input_array)
+        # Flatten for encoding, unflatten for learning
+        input_shape = learner_input.shape
+        flat_input = learner_input.flatten()
+        encoded_input = self.encoder.transform(flat_input)
+        encoded_input = encoded_input.reshape(input_shape)
+        predictions = self.svm.predict(encoded_input)
+        return predictions
+
+    def _evaluate_trees(self, input_array) -> np.ndarray:
+        # Can use sklearn decision trees, must catch potential nans before and skip eval
+
+        tree_result = []
+
+        for selected_features, tree in self.trees.items():  # Todo: Parralize
+            sub_feature_array = input_array[:, selected_features]
+            # Filter out Nans,
+            nan_mask = np.isnan(sub_feature_array).any(axis=1)
+            nan_array = np.where(nan_mask)
+            nan_result = np.ones(len(nan_array), dtype=object)
+            nan_result.fill(NAN)
+            clean_array = np.where(~nan_mask)
+            clean_input = sub_feature_array[clean_array]
+            # Predict non-Nans
+            if (clean_input.shape[0] > 0):  # Catch the empty case
+                clean_result = tree.predict(clean_input)
+            else:
+                clean_result = []
+            # Merge | Works only for 1D Data
+            final_result = np.zeros(len(input_array), dtype=object)
+            np.put(final_result, nan_array, nan_result)
+            np.put(final_result, clean_array, clean_result)
+            tree_result.append(final_result)
+
+        return np.array(tree_result).transpose()
 
 
-
-
-
-
-
-
-
-
-
-
+        # Todo: A proper string rep
--- test/test_forest.py
+++ test/test_forest.py
@@ -1,19 +1,49 @@
 import pytest
 import numpy as np
+from os.path import join, dirname
+import sys
+
+sys.path.append(join("..", "..", dirname(__file__)))
 
 from Forest import Forest
+
 
 @pytest.fixture(scope='module')
 def simple_single_data():
     return np.array([[1, 3, 5]]), np.array(["A"])
 
 
+@pytest.fixture(scope='module')
+def simple_data():
+    return np.array([[1, 3, 5], [100, 900, 110]]), np.array(["A", "B"])
 
+
+@pytest.fixture(scope='module')
+def simple_nan_single_data():
+    return np.array([[1, np.nan, 5]]), np.array(["A"])
+
+@pytest.fixture(scope='module')
+def simple_nan_data():
+    return np.array([[1, np.nan, 5], [100, 900, np.nan]]), np.array(["A", "B"])
 
 
 class TestAdhoc():
-
-    def test_single_data(self, simple_single_data):
-        forest=Forest(variant="ADHOC")
+    def test_single_data_ADHOC(self, simple_single_data):
+        forest = Forest(variant="ADHOC")
         forest.fit(simple_single_data[0], simple_single_data[1])
-        assert forest.predict(simple_single_data[0])==simple_single_data[1]
+        assert forest.predict(simple_single_data[0]) == simple_single_data[1]
+
+    def test_single_data_BOOSTED(self, simple_data):
+        forest = Forest(variant="BOOSTED")
+        forest.fit(simple_data[0], simple_data[1])
+        assert (forest.predict(simple_data[0]) == simple_data[1]).all() #Todo: Make deterministic
+
+    def test_simple_nan_data_ADHOC(self, simple_nan_single_data):
+        forest = Forest(variant="ADHOC")
+        forest.fit(simple_nan_single_data[0], simple_nan_single_data[1])
+        assert forest.predict(simple_nan_single_data[0]) == simple_nan_single_data[1]
+
+    def test_simple_nan_data_BOOSTED(self, simple_nan_data):
+        forest = Forest(variant="BOOSTED")
+        forest.fit(simple_nan_data[0], simple_nan_data[1])
+        assert (forest.predict(simple_nan_data[0]) == simple_nan_data[1]).all()
